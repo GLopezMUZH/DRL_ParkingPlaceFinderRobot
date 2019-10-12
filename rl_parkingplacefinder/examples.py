b1c0f9d6047c7e9f88596b27b52b4e1a2d55bda4
GLopezMUZH <gabrielaeugenia.lopezmagana@uzh.ch>
2019-10-13 00:00:36 +0200

    basic parking lot creation with examples

--- rl_parkingplacefinder/examples.py
+++ rl_parkingplacefinder/examples.py
@@ -1,11 +1,58 @@
-import rl_parkingplacefinder
-from rl_parkingplacefinder import Slot as Slot
-from rl_parkingplacefinder import ParkingLot as ParkingLot
+#%%
+#import rl_parkingplacefinder
+#from rl_parkingplacefinder import Slot as Slot
+#from rl_parkingplacefinder import ParkingLot as ParkingLot
+
+import networkx as nx
+
+# E D D D D D D  7 Entry 
+#   D P P P P D  6
+#   D D D D D D  5
+#   D P P P P D  4
+#   D D D D D D  3
+#   D P P P P D  2
+# E D D D D D D  1
+# x 1 2 3 4 5 6 
+# parking w=4, l=
+#%%
+nr_lanes = 7
+nr_parking_lanes = 3
+nr_places_in_lane = 6
 
 #%%
-4+4 
+# each place is a node, attribute type and attribute state
+
+g_grid = nx.grid_2d_graph(nr_lanes, nr_places_in_lane)
+g_grid = nx.convert_node_labels_to_integers(g_grid)
+
+node_color_map = []
+
+#only for single depth
+slot_nr = -1
+for lane in range(nr_lanes):
+    lane_nr = lane+1
+    for nr_slot_in_lane in range(nr_places_in_lane):
+        slot_nr += 1
+        # the parking lanes
+        if(lane_nr%2==0):
+            if((slot_nr%nr_places_in_lane == 0) or (slot_nr%nr_places_in_lane == nr_places_in_lane-1)):
+                g_grid.nodes[slot_nr]['slot_type'] = 'drive'
+                node_color_map.append('grey')
+            else:
+                g_grid.nodes[slot_nr]['slot_type'] = 'park'
+                node_color_map.append('green')
+        else:
+                g_grid.nodes[slot_nr]['slot_type'] = 'drive'
+                node_color_map.append('grey')
+
+g_grid.nodes.data()
+pos = nx.spring_layout(g_grid,iterations=500)
+nx.draw(g_grid, pos=pos,  node_color = node_color_map, with_labels=True)
 
 
 
 #%%
-slot = Slot('A1','P',0)
+#slot = Slot('A1','P',0)
+
+
+#%%
--- rl_parkingplacefinder/rl_parkingplacefinder.py
+++ rl_parkingplacefinder/rl_parkingplacefinder.py
@@ -1,3 +1,4 @@
+
 # %%
 """
     parkingplacefinder is an OpenSource python package for the reinforcement learning
@@ -12,7 +13,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
 """
-
+#%%
 import numpy as np
 import numpy.random as rnd
 
@@ -43,9 +44,9 @@ class Slot():
 
 
 class Filling_Function_Parameters():
-    def __init__(self):
-        self.filling_function = 'uniform'
-        self.uniform_distribution_p_value = 0.7
+    def __init__(self, filling_function = 'uniform', uniform_distribution_p_value = 0.7):
+        self.filling_function = filling_function
+        self.uniform_distribution_p_value = uniform_distribution_p_value
 
 
 class Lane_Direction_Parameters():
@@ -71,6 +72,7 @@ class ParkingLot():
     parking_lane_depth : input integer (default: 2) 
         Single depth = 1, double depth 0 2
     single_depth_outer_lanes: boolean (default: True)
+        Only relevant for double depth parking lanes.
         The first and last lane of the parking are only single depth parking slots
     lane_direction_paramenters : 
         Object of   
@@ -78,25 +80,73 @@ class ParkingLot():
         object of 
     """
 
-    def __init__(self, lane_direction_paramenters: Lane_Direction_Parameters, filling_function_parameters: Filling_Function_Parameters, w=10, l=1, parking_lane_depth=2, single_depth_outer_lanes=True, debug = False):
-        self.w = w
-        self.l = l
+    def __init__(self, lane_direction_paramenters: Lane_Direction_Parameters, filling_function_parameters: Filling_Function_Parameters, nr_parking_slots_per_lane=10, nr_parking_lanes=1, parking_lane_depth=2, single_depth_outer_lanes=True, debug = False, draw_grap = False, show_summary = False):
+        self.nr_parking_slots_per_lane = nr_parking_slots_per_lane
+        self.nr_parking_lanes = nr_parking_lanes
+        self.nr_parking_slots = nr_parking_slots_per_lane*nr_parking_lanes
         self.parking_lane_dept = parking_lane_depth
         self.single_depth_outer_lanes = single_depth_outer_lanes
         self.debug = debug
+        # internal variables
+        self.node_color_map = []
+        self.nr_occupied_parking_slots = 0
         # helps define structure
         self.lane_direction_paramenters = lane_direction_paramenters
         # create grid
-        number_of_lanes = self.get_number_of_lanes()
-        self.printDebug('number_of_lanes='+str(number_of_lanes))
-        # width = number of parking slots + 1 driveway places on each side
-        self.g = nx.grid_2d_graph(self.w+2, number_of_lanes)
-        self.g = nx.convert_node_labels_to_integers(self.g)
-
+        self.slots = [0]*(nr_parking_lanes*nr_parking_slots_per_lane)
+        self.nr_slots_per_lane = self.nr_parking_slots_per_lane+2
+        self.nr_lanes_total =  self.get_number_of_lanes()
+        self.nr_slots_total = self.nr_slots_per_lane*self.nr_lanes_total
+        self.printDebug('number_of_lanes='+str(self.nr_lanes_total))
+        self.create_parking_geography()
+        # update occupied places
         self.filling_function_parameters = filling_function_parameters
-
         self.fill_parking_slots()
-        self.set_lane_directions()
+        # set lane directions TODO
+        #self.set_lane_directions()
+
+        if draw_grap:
+            self.g.nodes.data()
+            pos = nx.spring_layout(self.g,iterations=1000)
+            nx.draw(self.g, pos=pos,  node_color = self.node_color_map, with_labels=True)
+        
+        if show_summary:
+            print("nr_parking_slots", self.nr_parking_slots)
+            print("nr_occupied_parking_slots = ", self.nr_occupied_parking_slots )
+            pct_message = 'percent occupation = ' + f"({(self.nr_occupied_parking_slots/self.nr_parking_slots):.3g})"
+            print(pct_message)
+
+
+    def create_parking_geography(self):
+        # width = number of parking slots + 1 driveway places on each side
+        self.g = nx.grid_2d_graph(self.nr_lanes_total, self.nr_slots_per_lane)
+        self.g = nx.convert_node_labels_to_integers(self.g)
+        if(self.parking_lane_dept==1):
+            self.create_parking_single_depth()
+
+
+    def create_parking_single_depth(self):
+        #only for single depth, we always start with drive path
+        slot_nr_in_graph = -1
+        for lane in range(self.nr_lanes_total):
+            lane_nr = lane+1
+            for slot_nr_in_lane in range(self.nr_slots_per_lane):
+                slot_nr_in_graph += 1
+                # the parking lanes
+                if(lane_nr%2==0):
+                    # the first and last slot are always drive slots
+                    if((slot_nr_in_graph%self.nr_slots_per_lane == 0) or (slot_nr_in_graph%self.nr_slots_per_lane == self.nr_slots_per_lane-1)):
+                        self.g.nodes[slot_nr_in_graph]['slot_type'] = 'drive'
+                        self.node_color_map.append('grey')
+                    else:
+                        self.g.nodes[slot_nr_in_graph]['slot_type'] = 'park'
+                        self.node_color_map.append('green')
+                else:
+                        self.g.nodes[slot_nr_in_graph]['slot_type'] = 'drive'
+                        self.node_color_map.append('grey')
+
+
+
 
     def printDebug(self, *val):
         if self.debug:
@@ -111,10 +161,15 @@ class ParkingLot():
         Basic function to find total number of aisles (parking and driving) based on the given number of
         parking aisles
         """
+        # single depth parking lanes
+        if(self.parking_lane_dept==1):
+            return 2*self.nr_parking_lanes + 1
+
+        # double depth parking lanes
         if(self.single_depth_outer_lanes):
-            return 2*self.l + 1
+            return 2*self.nr_parking_lanes + 1
         else:
-            return math.ceil(self.l/2)+self.l
+            return math.ceil(self.nr_parking_lanes/2)+self.nr_parking_lanes
 
     def set_lane_directions(self):
         if (self.lane_direction_paramenters.directed_lanes):
@@ -137,10 +192,12 @@ class ParkingLot():
             return self.fill_parking_uniform()
 
     def fill_parking_uniform(self):
-        for i in range(self.w*self.l):
-            rn = rnd.random()
-            if (rn < self.filling_function_parameters.uniform_distribution_p_value):
-                print('place occupied')
+        for i in range(self.nr_slots_total):
+            if(self.g.nodes[i]['slot_type'] == 'park'):
+                rn = rnd.rand()
+                if (rn < self.filling_function_parameters.uniform_distribution_p_value):
+                    self.node_color_map[i]='red'
+                    self.nr_occupied_parking_slots += 1
 
     def export_parking_lot_data(self):
         """
@@ -155,5 +212,9 @@ class ParkingLot():
             writer = csv.writer(f)
             writer.writerows(data)
 
-
 # %%
+class Park_Finder_Agent():
+    def __init__(self):
+        #row = agent//self.nr_parking_lanes
+        #col = agent%self.nr_parking_lanes
+        self.id_name = 'hola'
