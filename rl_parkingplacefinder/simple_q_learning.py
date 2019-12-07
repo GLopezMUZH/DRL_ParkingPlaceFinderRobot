feb99c46d5e1de6ff0f08573b567b33a336f28b7
GLopezMUZH <gabrielaeugenia.lopezmagana@uzh.ch>
2019-12-07 12:26:11 +0100

    set current working directory automatically from file path

--- rl_parkingplacefinder/simple_q_learning.py
+++ rl_parkingplacefinder/simple_q_learning.py
@@ -24,7 +24,7 @@ import scipy.misc
 import os
 
 os.getcwd()
-os.chdir('c:\\Users\\glopez\\uzh_stuff\\DeepReinforcementLearning\\DRL_ParkingPlaceFinderRobot\\rl_parkingplacefinder')
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 import Parking_lot
 from Parking_lot import Parking_Lot
